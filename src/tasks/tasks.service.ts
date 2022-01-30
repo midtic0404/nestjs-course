1a1e2060e85f222ee7592f5583110cc439bfb70e
Chia Che Chang <midtic0404@gmail.com>
2022-01-29 22:06:36 -0700

    stuffs

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDTO } from './dto/create-task.dto';
@@ -36,9 +36,15 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => {
+    const found = this.tasks.find((task) => {
       return task.id === id;
     });
+
+    if (!found) {
+      throw new NotFoundException(`Task with id ${id} is not found.`);
+    }
+
+    return found;
   }
 
   createTask(createTaskDTO: CreateTaskDTO): Task {
